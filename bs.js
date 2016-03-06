5dfb33ee82a62fe0da0e566cce73edcc37b61e48
Haroen Viaene <fingebimus@me.com>
2016-03-06 13:16:31 +0100

    fix jquery dependency
    Since this is fairly basic, this can be accomplished without jquery as well :smile:

--- bs.js
+++ bs.js
@@ -1,7 +1,11 @@
-$(function(){
-  $(window).on('blur focus focusin focusout load resize scroll unload click ' +
-      'dblclick mousedown mouseup mousemove mouseover mouseout mouseenter ' + 
-       'mouseleave change select submit keydown keypress keyup error', function(e){
-      console.log( e.type + ' is bullshit!')
-  });
-});
+(function() {
+	var addListener = function(type) {
+		window.addEventListener(type, function(e) {
+			console.log(e.type + ' is bullshit!')
+		});
+	}
+	var events = ["blur", "focus", "focusin", "focusout", "load", "resize", "scroll", "unload", "click", "dblclick", "mousedown", "mouseup", "mousemove", "mouseover", "mouseout", "mouseenter", "mouseleave", "change", "select", "submit", "keydown", "keypress", "keyup", "error"];
+	for (var i in events) {
+		addListener(events[i]);
+	}
+})();
